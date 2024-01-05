f8ea9984aa2d03b75b826a594b980a8f98576250
ndambuki8 <ndambukikevo8@gmail.com>
2024-01-05 04:38:37 +0300

    generator expression

--- test.py
+++ test.py
@@ -1,21 +1,8 @@
-#Python generators
-def my_generator(n):
+#Python generators expression - a concise way to create a generator object
 
-    #initialize counter
-    value = 0
+#create the generator object
+squares_generator = (i * i for i in range(5))
 
-    #loop until counter is less than n
-    while value < n:
-
-        yield value
-
-        value += 1
-
-    
-# for value in my_generator(3):
-#     print(value)
-
-generator = my_generator(3)
-print(next(generator))
-print(next(generator))
-print(next(generator))
+#iterate over the generator and print the values
+for i in squares_generator:
+    print(i)
