b5db7949f7d75a20c28dd9a91e00a537a0ee8c58
ndambuki8 <ndambukikevo8@gmail.com>
2024-01-11 08:03:43 +0300

    python closures example 2

--- test.py
+++ test.py
@@ -1,8 +1,18 @@
 #Python closures
-def greet(name):
-    name = 'John'
-    return lambda : "Hi " + name
+def calculate():
+    num = 1
+    def inner_func():
+        nonlocal num
+        num += 2
+        return num
+    return inner_func
 
-message = greet()
+#call the outer function
+odd = calculate()
+print(odd())
+print(odd())
+print(odd())
 
-print(message())
+#call outer functioo again
+odd2 = calculate()
+print(odd2())
