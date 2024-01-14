1183ef54059a39479c8d504adc4f9995c11864bf
ndambuki8 <ndambukikevo8@gmail.com>
2024-01-14 12:10:06 +0300

    creating objects from celsius class--@property

--- test.py
+++ test.py
@@ -5,3 +5,15 @@ class Celsius:
 
     def to_fahrenheit(self):
         return (self.temperature * 1.8) + 32
+
+#create a new object 
+human = Celsius()
+
+#set the temperature
+human.temperature = 37
+
+#Get the teamperature attribute
+print(human.temperature)
+
+#get the fahrenheit method
+print(human.to_fahrenheit())
