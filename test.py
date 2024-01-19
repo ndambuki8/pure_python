c434a99e00f4c213cc11d8c6a4ca21c17abdb884
ndambuki8 <ndambukikevo8@gmail.com>
2024-01-19 08:48:48 +0300

    adding property object

--- test.py
+++ test.py
@@ -1,34 +1,22 @@
 #Python @property decorators
 class Celsius:
     def __init__(self, temperature = 0):
-        self.set_temperature(temperature)
+        self.temperature = temperature
 
     def to_fahrenheit(self):
-        return (self.get_temperature() * 1.8) + 32
+        return (self.temperature * 1.8) + 32
     
     #getter method
     def get_temperature(self):
+        print("Getting value.....")
         return self._temperature #underscore at the beginning is used to denote private variables
     
     #setter method
     def set_temperature(self, value):
+        print("Setting value...")
         if value < -273.15:
             raise ValueError("Temperature below -273.15 is not possible")
         self._temperature = value
 
-#create a new object 
-human = Celsius(37)
-
-#set the temperature
-# human.temperature = 37
-
-#Get the teamperature attribute
-print(human.get_temperature())
-
-#get the fahrenheit method
-print(human.to_fahrenheit())
-
-#constraint test
-print(human.set_temperature(-300))
-
-print(human.__dict__)
+    #create a property object
+    temperature = property(get_temperature, set_temperature)
