2752143deb5d2314738402b3f487c4253b15abe1
ndambuki8 <ndambukikevo8@gmail.com>
2024-01-27 12:22:51 +0300

    Raw string using r prefix

--- test.py
+++ test.py
@@ -1,19 +1,7 @@
-#RegEx 
-#- group()  - returns the part of the string where there is a match
-
 import re
+#Raw string using r prefix
 
-string = '39801 3565, 2102 1111'
+string = '\n and \r are escape sequences'
 
-pattern = '(\d{3}) (\d{2})'
-
-
-match = re.search(pattern, string)
-
-if match:
-    print(match.groups())
-    print(match.start())
-    print(match.end())
-    print(match.span())
-else:
-    print("Pattern not found")
+result = re.findall(r'[\n\r]', string)
+print(result)
