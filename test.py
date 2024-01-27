5ac717193d430bb9d490f280941f26b6e792a4a9
ndambuki8 <ndambukikevo8@gmail.com>
2024-01-27 11:55:57 +0300

    group()  - returns the part of the string where there is a match

--- test.py
+++ test.py
@@ -1,15 +1,16 @@
 #RegEx 
-#- re.search(patterb, str)
+#- group()  - returns the part of the string where there is a match
 
 import re
 
-string = 'Python is fin'
+string = '39801 3565, 2102 1111'
 
-#check if 'Python' isat the beginning 
-match = re.search('\APython', string)
+pattern = '(\d{3}) (\d{2})'
+
+
+match = re.search(pattern, string)
 
 if match:
-    print("pATTERN FOUND INSUDE")
+    print(match.groups())
 else:
     print("Pattern not found")
-
