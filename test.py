00c75692df0513288d003f5f6d1ea903fd31b063
ndambuki8 <ndambukikevo8@gmail.com>
2024-02-03 10:01:19 +0300

    instance(a, b) vs type()

--- test.py
+++ test.py
@@ -1,8 +1,18 @@
-#getting the class name of an instance
+#difference between type() and instance()
 
-class Vehicle:
-    def name(self, name):
-        return name
-    
-v = Vehicle()
-print(type(v).__name__)
+class Polygon:
+    def sides_no(self):
+        pass
+
+class Triangle(Polygon):
+    def area(self):
+        pass
+
+obj1 = Polygon()
+obj2 = Triangle()
+
+print(type(obj2) == Triangle)
+print(type(obj2) == Polygon)
+
+print(isinstance(obj1,Polygon))
+print(isinstance(obj2, Polygon))
