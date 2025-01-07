cd237c246e999f6f2549c91e86554ef0bf9f7819
ndambuki8 <ndambukikevo8@gmail.com>
2025-01-07 05:30:29 +0300

    two pass hash table

--- ds_practice.py
+++ ds_practice.py
@@ -1,11 +1,19 @@
 class Solution:
     def twoSum(self, nums, target):
+        numMap = {}
         n = len(nums)
-        for i in range(n-1):
-            for j in range(i + 1, n):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
-        return []
+
+        #Build the hash table
+        for i in range(n):
+            numMap[nums[i]] = i
+
+        #Find the complement
+        for i in range(n):
+            complement = target - nums[i]
+            if complement in numMap and numMap[complement] != i:
+                return [i, numMap[complement]]
+
+        return[]
 
 nums = [3,2,4]
 target = 6
