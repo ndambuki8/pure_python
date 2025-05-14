141eaf76e612208e02cd31f6f43a8019b6b502d0
ndambuki8 <ndambukikevo8@gmail.com>
2025-05-14 04:24:55 +0300

    using and understanding __new__

--- programiz/intro.py
+++ programiz/intro.py
@@ -1,17 +1,40 @@
-class Cat:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+class Point(object):
+    def __new__(cls, *args, **kwargs):
+        print("From new")
+        print(cls)
+        print(args)
+        print(kwargs)
 
-    def info(self):
-        print(f"I am a cat. My name is {self.name}. I am {self.age} years old")
+        # create our object and return it
+        obj = super().__new__(cls)
+        return obj
+    
+    def __init__(self, x=0, y=0):
+        print("From init")
+        self.x = x
+        self.y = y
 
-    def make_sound(self):
-        print("Meow")
 
+# p2 = Point(3,4)
+# p2
 
-cat1 = Cat('Andy', 2)
-cat2 = Cat("Phoebe", 3)
+class SqPoint(Point):
+    MAX_Inst = 4
+    Inst_created = 0
 
-cat1.info()
-cat2.make_sound()
+    def __new__(cls, *args, **kwargs):
+        if (cls.Inst_created >= cls.MAX_Inst):
+            raise ValueError("Cannot create more objects")
+        cls.Inst_created += 1
+        return super().__new__(cls)
+    
+p1 = SqPoint(0,0)
+p1
+p2 = SqPoint(1,0)
+p2
+p3 = SqPoint(1,1)
+p3
+p4 = SqPoint(0,1)
+p4
+p5 = SqPoint(2,2)
+p5
