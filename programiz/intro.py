9630d87566954a237518342a4c4a5da146cb7cfe
ndambuki8 <ndambukikevo8@gmail.com>
2025-05-29 02:19:21 +0300

    working with a nested loop in a list

--- programiz/intro.py
+++ programiz/intro.py
@@ -1,5 +1,9 @@
-number = (3-4j)
+student = ["Jack", 32, 'Computer Science', [2,4]]
+# print(student)
 
-absolute_number = abs(number)
-
-print(absolute_number)
+for item in student:
+    if item == 32:
+        continue
+    for it in item:
+        print(it)
+    # print(item)
