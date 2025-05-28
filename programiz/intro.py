513c3a1b1c26ff84a53ee176031448866ec671f3
ndambuki8 <ndambukikevo8@gmail.com>
2025-05-29 02:33:00 +0300

    played around with list methods, more to come

--- programiz/intro.py
+++ programiz/intro.py
@@ -1,9 +1,16 @@
 student = ["Jack", 32, 'Computer Science', [2,4]]
-# print(student)
 
-for item in student:
-    if item == 32:
-        continue
-    for it in item:
-        print(it)
-    # print(item)
+student.append(77)
+student.insert(3, "Buda")
+student[3] = "Jabaaa"
+student.remove(77)
+
+del student[0]
+del student[1:4]
+
+more = ["Jane", 45, "loves cooking", [24,8]]
+
+student.extend(more)
+print(student)
+# print(student[-5])
+# print(student[0])
