1e5fc93dd8a077cbdf64932140b1fb24ada63be4
ndambuki8 <ndambukikevo8@gmail.com>
2025-12-28 01:19:07 +0300

    50 days of python

---
+++ 50_days/day_1b.py
@@ -0,0 +1,7 @@
+def longest_value(d: dict):
+    # using max and key len to get the longest value
+    longest = max(d.values(), key=len)
+    return longest
+
+fruits = {'fruits': 'apple', 'color':'green'}
+print(longest_value(fruits))
