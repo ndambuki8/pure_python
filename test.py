186c2a620544219ef4199038cdd1d3d20e348aa0
ndambuki8 <ndambukikevo8@gmail.com>
2025-12-28 15:36:38 +0300

    special methods from Python Data Model to build new classes

--- test.py
+++ test.py
@@ -1,35 +1,25 @@
-import collections
+import math
 
-Card = collections.namedtuple('Card', ['rank', 'suit'])
+class Vector:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
 
-class FrenchDeck:
-    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
-    suits = 'spades diamonds clubs hearts'.split()
-
-    def __init__(self):
-        self._cards = [Card(rank, suit) for suit in self.suits
-                                        for rank in self.ranks]
-        
-    def __len__(self):
-        return len(self._cards)
+    def __repr__(self):
+        return f'Vector({self.x!r}, {self.y!r})'
     
-    def __getitem__(self, position):
-        return self._cards[position]
+    def __abs__(self):
+        return math.hypot(self.x, self.y)
     
-
-from random import choice
-
-deck = FrenchDeck()
-print(len(deck))
-
-print(choice(deck))
-print(Card('Q', 'hearts') in deck)
-
-suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
-
-def spades_high(card):
-    rank_value =  FrenchDeck.ranks.index(card.rank)
-    return rank_value * len(suit_values[card.suit])
-
-for card in sorted(deck, key=spades_high):
-    print(card)
+    def __bool__(self):
+        return bool(abs(self))
+    
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return Vector(x, y)
+    
+    def __mul__(self, scalar):
+        return Vector(self.x * scalar, self.y * scalar)
+    
+    
