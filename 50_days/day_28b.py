5d2fe6cce374ae094011cd4b8c7b0f5bd41a47e3
ndambuki8 <ndambukikevo8@gmail.com>
2026-01-22 10:54:43 +0300

    50 days of python

---
+++ 50_days/day_28b.py
@@ -0,0 +1,17 @@
+def largest_number(arr: list):
+    # Create empty list
+    list1 = []
+    # remove brackets and spaces in the lists
+    n = str(arr).strip('[,]').replace(',', '').replace(' ', '')
+    # append the numbers to the empty list
+    for i in n:
+        list1.append(int(i))
+        # Sorting the list in descending order
+
+    list1.sort(reverse=True)
+    # removing parenthesis and spaces from the sorted 
+    # list and converting it to int
+    large_number = int(str(list1).strip('[]').replace(',', '').replace(' ', ''))
+    return 'The largest number is, {:,}'.format(large_number)
+
+n = [3,67,87,9,2]
